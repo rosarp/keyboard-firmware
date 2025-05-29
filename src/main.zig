a88e072982238e5ac9f4b7d8a44da7e2cb7840a9
Rohit Sarpotdar <rohi7ns@gmail.com>
2025-05-29 22:54:28 +0530

    updated zig min version to 0.14.1 updated microzig dependency

--- src/main.zig
+++ src/main.zig
@@ -4,15 +4,20 @@ const info = std.log.info;
 const microzig = @import("microzig");
 const gpio = microzig.hal;
 
-// const keyboard_config = @import("build_options").keyboard_config;
+const gpio_keycode_map = @import("build_options").gpio_keycode_map;
+const Scanner = @import("keyscan.zig").Scanner;
 
-// comptime load left or right gpio pins
+var active_layer: u8 = 0;
 
-pub fn main() !void {}
+pub fn main() !void {
+    // const gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // defer _ = gpa.deinit();
+    // create Keyboard
+}
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
